6103b22f79b1ba54b3c78bdb313dd9185767101b
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-08 13:35:26 -0300

    feat: create routes to start and end Prismic Preview mode

--- src/pages/api/exit-preview.ts
+++ src/pages/api/exit-preview.ts
@@ -1,6 +1,6 @@
 import { NextApiResponse } from 'next';
 
-export default async (res: NextApiResponse): Promise<void> => {
+export default async (_, res: NextApiResponse): Promise<void> => {
   res.clearPreviewData();
 
   res.writeHead(307, { Location: '/' });
